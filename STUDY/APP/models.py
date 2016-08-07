44232b9b4f35c54788253d589810e455787ec6e2
정준영 <jjy920517@gmail.com>
2016-08-07 15:24:17 +0900

    [16. 08. 07] admin페이지, 템플릿 생성

--- STUDY/APP/models.py
+++ STUDY/APP/models.py
@@ -1,5 +1,11 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=10)
+    introduction = models.TextField()
+    area = models.CharField(max_length=15)
+    party_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- STUDY/APP/views.py
+++ STUDY/APP/views.py
@@ -1,6 +1,15 @@
+#-*- coding: utf-8 -*-
+#-*- coidng: euc-kr -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Candidate
 
-# Create your views here.
+# 한글사용
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 def index(request):
-    return HttpResponse("Hello world")
+    candidates = Candidate.objects.all()
+    return render(request, 'APP/index.html')
