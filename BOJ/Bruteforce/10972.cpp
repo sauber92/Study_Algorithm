7ed8a20e1aab86cef0d89e218998e1f1100071c6
sauber92 <jjy920517@gmail.com>
2019-05-15 16:30:02 +0900

    Update BOJ/Bruteforce/10972.cpp

---
+++ BOJ/Bruteforce/10972.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, num;
+    vector<int> in;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+        in.push_back(num);
+    }
+
+    if (next_permutation(in.begin(), in.end())) {
+        for (int i = 0; i < N; i++) {
+            cout << in[i] << " ";
+        }
+        cout << "\n";
+    }
+    else {
+        cout << "-1\n";
+    }
+
+    return 0;
+}
