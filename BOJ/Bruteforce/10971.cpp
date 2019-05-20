00e5ad84f26d0bd6ccf2c8f0e8914d65edb112d2
sauber92 <jjy920517@gmail.com>
2019-05-20 23:31:41 +0900

    Update BOJ/Bruteforce/10971.cpp

---
+++ BOJ/Bruteforce/10971.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1000000 * 4;
+
+int main() {
+    int n, ans = MAX;
+    scanf("%d", &n);
+
+    vector<int> d(n);
+    int w[n][n];
+
+    for (int i = 0; i < n; i++) {
+        d[i] = i;
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &w[i][j]);
+        }
+    }
+
+    do {
+        bool ok = true;
+        int sum = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            if (w[d[i]][d[i+1]] == 0) {
+                ok = false;
+            }
+            else {
+                sum += w[d[i]][d[i+1]];
+            }
+
+            if (ok && w[d[n-1]][d[0]] != 0) {
+                sum += w[d[n-1]][d[0]];
+                ans = min(ans, sum);
+            }
+        }
+
+    } while (next_permutation(d.begin(), d.end()));
+
+    printf("%d\n", ans);
+    
+    return 0;
+}
